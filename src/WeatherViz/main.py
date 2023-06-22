18289bf9dc6a80feef5d584d82bdfba0affef4d1
nancylucy01 <nancylucy02@gmail.com>
2023-06-22 11:22:05 +0900

    Fix main.py

--- src/WeatherViz/main.py
+++ src/WeatherViz/main.py
@@ -1,16 +1,14 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from PySide2.QtWidgets import QApplication
 from gui.mainwindow import MainWindow
-import PyQt5.QtWidgets
 
 
 def main():
     app = QApplication([])
     app.setStyle('Fusion')
     window = MainWindow()
-    #window.display()
     window.resize(1250, 750)
     window.show()
-    app.exec()
+    app.exec_()
 
 
 # Press the green button in the gutter to run the script.
