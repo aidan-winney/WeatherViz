d5002a36a76c4589b1bf724397e9fea473da8296
Nic Butakow <nicholas@adastralab.com>
2023-06-26 02:06:09 +0900

    Add geocoordinate grid calculator

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,47 @@
 use pyo3::prelude::*;
-use reqwest;
-use pyo3::exceptions::PyRuntimeError;
-use pyo3::types::IntoPyDict;
 
-#[pyfunction]
-fn get_data(py: Python, start_date: String, end_date: String) -> PyResult<PyObject> {
-    let url = format!("https://archive-api.open-meteo.com/v1/archive?latitude=29.68&longitude=-82.34&start_date={}&end_date={}&hourly=temperature_2m&min={}&max={}", start_date, end_date, start_date, end_date);
+const PI: f64 = std::f64::consts::PI;
+const DEG_TO_RAD: f64 = PI / 180.0;
+const MAX_LAT: f64 = 85.0511287798; // arctan(sinh(pi))
+const TRN_SCALE: f64 = 0.5 / PI;
 
-    let client = reqwest::blocking::Client::new();
-    let response = client.get(&url).send().map_err(|err| PyRuntimeError::new_err(format!("{}", err)))?;
-
-    if response.status().is_success() {
-        let text = response.text().map_err(|err| PyRuntimeError::new_err(format!("{}", err)))?;
-        let data: serde_json::Value = serde_json::from_str(&text).map_err(|err| PyRuntimeError::new_err(format!("{}", err)))?;
-
-        let locals = [("data", data.to_string())].into_py_dict(py);
-        py.run("import json; result = json.loads(data)", Some(locals), None)?;
-
-        let python_data = locals.get_item("result").ok_or_else(|| PyRuntimeError::new_err("Failed to get 'result'"))?.to_object(py);
-
-        Ok(python_data)
-    } else {
-        Err(PyRuntimeError::new_err(format!("It didn't work. Status code: {}", response.status())))
-    }
+fn scale(zoom: i32) -> f64 {
+    256.0 * 2.0_f64.powf(zoom as f64)
 }
 
+// geocoords to coords on The Big Map in pixel space
+fn project(lat: f64, lon: f64, zoom: i32) -> (f64, f64) {
+    let sin = (MAX_LAT.min(lat).max(-MAX_LAT) * DEG_TO_RAD).sin();
+    let zoom_scale = scale(zoom);
+    let (x, y) = (lon * DEG_TO_RAD, ((1.0 + sin) / (1.0 - sin)).ln() / 2.0);
+    (zoom_scale * (TRN_SCALE * x + 0.5), zoom_scale * (0.5 - TRN_SCALE * y))
+}
+
+// get geocoords of the points in a res x res lattice in pixel space
+#[pyfunction]
+fn geocoords(width: i32, height: i32, res: i32, lat: f64, lon: f64, zoom: i32) -> Vec<(f64, f64)> {
+    let width = width as f64;
+    let height = height as f64;
+    let inverse = 1.0 / (res as f64);
+    let offset = (inverse - 1.0) / 2.0;
+    let zoom_scale = scale(zoom);
+    let (x_i, y_i) = project(lat, lon, zoom);
+    let mut coords = Vec::new();
+    for i in 0..res {
+        let i = i as f64;
+        for j in 0..res {
+            let j = j as f64;
+            let x = ((x_i + width * (i * inverse + offset)) / zoom_scale - 0.5) / TRN_SCALE;
+            let y = ((y_i + height * (j * inverse + offset)) / zoom_scale - 0.5) / -TRN_SCALE;
+            coords.push(((2.0 * y.exp().atan() - PI / 2.0) / DEG_TO_RAD, x / DEG_TO_RAD));
+        }
+    }
+    coords
+}
 
 #[pymodule]
-#[pyo3(name = "renderer")]
-fn renderer(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_data, m)?)?;
+#[pyo3(name="renderer")]
+fn renderer(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(geocoords, m)?)?;
     Ok(())
 }
